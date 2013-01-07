17d4293989141354d37a6236367b8c08fc37c14b
chrox <chrox.huang@gmail.com>
2013-01-07 23:29:43 +0800

    add extractors for Kindle Touch

---
+++ README_FIRST.txt
@@ -0,0 +1,15 @@
+KPVBooklet for Kindle Touch and Kindle Paperwhite
+======================================
+
+KPVBooklet is a Kindle booklet for starting kindlepdfviewer
+and updating last access and percentage finished information
+in Kindle content catalog entry of the opened document.
+
+Supported firmware versions are:
+	* Kindle Touch 5.1.2
+	* Kindle Paperwhite 5.2.0
+ 	* Kindle Paperwhite 5.3.0
+	* Kindle Paperwhite 5.3.1
+
+KPVBooklet is licensed under the MIT license. See the file
+LICENSE for more details.
--- build-updates.sh
+++ build-updates.sh
@@ -2,7 +2,7 @@
 
 HACKNAME="kpvbooklet"
 PKGNAME="${HACKNAME}"
-PKGVER="20130107"
+PKGVER="0.2.N"
 
 # We need kindletool (https://github.com/NiLuJe/KindleTool) in $PATH
 if (( $(kindletool version | wc -l) == 1 )) ; then
@@ -24,16 +24,18 @@ fi
 ## Move stuff around a bit to have everything in the right place in the least amount of steps, and without duplicating stuff in the repo
 [ ! -d build/install ] && mkdir build/install
 [ ! -d build/uninstall ] && mkdir build/uninstall
+
 cp -f install.ffs build/install/install.ffs
-cp -f uninstall.ffs build/uninstall/uninstall.ffs
-cp -f mimes.sql build/install/mimes.sql
 cp -f build/jar/KPVBooklet.jar build/install/KPVBooklet.jar
+cp -f mimes.sql build/install/mimes.sql
+
+cp -f uninstall.ffs build/uninstall/uninstall.ffs
 
 ## K5
 # Install
-kindletool create ota2 -d k5w -d k5g -d k5gb -d pw -d pwg -d pwgb -C build/install update_${PKGNAME}-${PKGVER}_install-k5.bin
+kindletool create ota2 -d k5w -d k5g -d k5gb -d pw -d pwg -d pwgb -C build/install update_${PKGNAME}_${PKGVER}_install.bin
 # Uninstall
-kindletool create ota2 -d k5w -d k5g -d k5gb -d pw -d pwg -d pwgb -C build/uninstall update_${PKGNAME}-${PKGVER}_uninstall-k5.bin
+kindletool create ota2 -d k5w -d k5g -d k5gb -d pw -d pwg -d pwgb -C build/uninstall update_${PKGNAME}_${PKGVER}_uninstall.bin
 
-zip -m kindle-kpvbooklet-${PKGVER} *.bin
+zip kindle-kpvbooklet-${PKGVER} *.bin README_FIRST.txt ChangeLog.txt
 
