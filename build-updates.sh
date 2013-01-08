0115e6d1e62c8ab7ace2a226b5108169c947f1bb
chrox <chrox.huang@gmail.com>
2013-01-08 11:18:11 +0800

    overwrite dest dir when installing new extractors

--- build-updates.sh
+++ build-updates.sh
@@ -2,7 +2,7 @@
 
 HACKNAME="kpvbooklet"
 PKGNAME="${HACKNAME}"
-PKGVER="0.2.N"
+PKGVER="0.3.1"
 
 # We need kindletool (https://github.com/NiLuJe/KindleTool) in $PATH
 if (( $(kindletool version | wc -l) == 1 )) ; then
